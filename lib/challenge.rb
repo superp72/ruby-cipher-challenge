906b85881109c936be26f5d25ea38b22bc1777b1
Paul Peterson <superp72pp@gmail.com>
2016-07-07 11:19:03 +1000

    first commit hw w08d03

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,10 +1,41 @@
+cypher = { 'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q', 'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u', 'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y', 'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c', 'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g', 'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k', 'y' => 'l', 'z' => 'm'}
 
-def encode(string, rot=13)
+def encode(string, cypher)
   string.downcase!
-  # Your Code Goes Here
+  string = string.split('')
+  newstring = []
+  string.each do |letter|
+  	
+	newstring.push(cypher[letter])
+	
+  end
+  puts newstring.join
 end
+encode('Hello', cypher)
 
-def decode(string, rot=13)
-  string.downcase!
-  # Your Code Goes Here
-end
+
+
+# for string = i ; i.length
+
+# split the string
+# 'h' , 'e' , 'l' ...etc
+
+# for each in the new split array,
+# 	ex: letter = cypher['h']
+# get the corresponding cypher for the letter
+# 'u', .. etc
+
+# combine the split cyphered letters back together
+# str[start, length] → new_str 
+# .each |do|
+
+# b = Array.new
+ # b = string.split('')
+#   puts b
+
+# #ary[start, length] → new_ary or nil
+# # encode('world')
+# # def decode(string, rot=13)
+# #   string.downcase!
+# #   # Your Code Goes Here
+# # end
